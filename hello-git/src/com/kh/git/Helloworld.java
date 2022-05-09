8109332fad5f6f44347352d724671ef1460397d4
ymiru0324 <ymiru0324@naver.com>
2022-05-09 10:49:36 +0900

    add Cat, cry

--- hello-git/src/com/kh/git/Helloworld.java
+++ hello-git/src/com/kh/git/Helloworld.java
@@ -1,5 +1,6 @@
 package com.kh.git;
 
+import com.kh.animal.Cat;
 import com.kh.animal.Dog;
 
 /*
@@ -22,5 +23,7 @@ public class Helloworld {
 		
 		Dog dog = new Dog();
 		dog.bark();
+		Cat cat = new Cat();
+		cat.cry();
 	}
 }
